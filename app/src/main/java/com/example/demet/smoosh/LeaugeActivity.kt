b335495a1156f40a0ea7248a0e70eac7301a1cc1
Demeter Gabor <gabor.demeter@attentioncrm.hu>
2018-01-23 15:15:01 +0100

    lesson2

---
+++ app/src/main/java/com/example/demet/smoosh/LeaugeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demet.smoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaugeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leauge)
+    }
+}
