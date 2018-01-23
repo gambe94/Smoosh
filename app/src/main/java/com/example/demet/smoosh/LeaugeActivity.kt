756c360196cc3aef10a48a574881f8cb707a7792
Demeter Gabor <gabor.demeter@attentioncrm.hu>
2018-01-23 16:16:26 +0100

    lesson6

--- app/src/main/java/com/example/demet/smoosh/LeaugeActivity.kt
+++ app/src/main/java/com/example/demet/smoosh/LeaugeActivity.kt
@@ -1,12 +1,20 @@
 package com.example.demet.smoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeaugeActivity : AppCompatActivity() {
+class LeaugeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leauge)
+    }
+
+    fun leaugeNextClicked(view: View){
+        val skilIntent = Intent(this, SkillActivity::class.java)
+        startActivity(skilIntent)
+
     }
 }
--- app/src/main/java/com/example/demet/smoosh/MainActivity.kt
+++ app/src/main/java/com/example/demet/smoosh/MainActivity.kt
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         getStartedbutton.setOnClickListener{
             val leaugeIntent = Intent(this,LeaugeActivity::class.java)
             startActivity(leaugeIntent)
-            
+
         }
     }
 }
